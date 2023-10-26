ec39fc3637f79088704e524df2810f2f9a7b1237
Ganesh Kasture <ganeshkasture9595@gmail.com>
2023-10-26 12:46:44 +0530

    01
    Files structure added

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './header.css'
+
+const Header = () => {
+  return (
+    
+    <nav>
+      <h1>Hi we are EduAccess pioners</h1>
+      <h5>home</h5>
+      <h5>moon</h5>
+      <h5>mun</h5>
+    </nav>
+  )
+}
+
+export default Header
