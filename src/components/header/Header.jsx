2406fafd08351436bc8291e7ab9008f7539452e3
Ganesh Kasture <ganeshkasture9595@gmail.com>
2023-11-06 11:53:27 +0530

    03
    added first page

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,13 +3,55 @@ import './header.css'
 
 const Header = () => {
   return (
+    <div className="div">
+    <div className="child" />
+    <div className="item" />
+    <div className="inner" />
+    <div className="rectangle-div" />
+    <div className="home">HOME</div>
+    <div className="services">services</div>
+    <div className="contact">contact</div>
+    <div className="child1" />
+    <div className="login">Login</div>
+    <img className="image-2-icon" alt="" src="/image-2@2x.png" />
+    <div className="t-o-g-container">
+      <span className="t-o-g-container1">
+        <p className="t-o">T O G E T H E R T O W A R D S</p>
+        <p className="t-o">&nbsp;</p>
+        <p className="t-o">{`H  O  R  I  Z  O  N     `}</p>
+      </span>
+    </div>
+    <div className="subscriptions">SUBSCRIPTIONS</div>
+    <div className="child2" />
+    <div className="personalized-education">Personalized Education</div>
+    <div className="increased-course-variety">Increased Course Variety</div>
+    <div className="career-advancement-opportuniti">
+      Career Advancement Opportunities
+    </div>
+    <div className="child3" />
+    <img
+      className="solarmenu-dots-linear-icon"
+      alt=""
+      src="/solarmenudotslinear.svg"
+    />
+    <img className="vector-icon" alt="" src="/vector.svg" />
+    <div className="previous">PREVIOUS</div>
+    <img className="vector-icon1" alt="" src="/vector1.svg" />
+    <div className="next">NEXT</div>
+    <img
+      className="material-symbolssearch-icon"
+      alt=""
+      src="/materialsymbolssearch.svg"
+    />
+    <img
+      className="carbonadd-filled-icon"
+      alt=""
+      src="/carbonaddfilled.svg"
+    />
+    <div className="ellipse-div" />
+  </div>
+
     
-    <nav>
-      <h1>Hi we are EduAccess pioners</h1>
-      <h5>home</h5>
-      <h5>moon</h5>
-      <h5>mun</h5>
-    </nav>
   )
 }
 
